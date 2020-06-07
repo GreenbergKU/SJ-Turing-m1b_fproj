62ca386787c1e47edd3672693e0b5b07620ed9bb
Suzie Q <suzieqwolf@gmail.com>
2020-06-06 18:42:24 -0600

    Adds calTurn, toggleTurn, and playCard and cleans up the comments

--- src/Player.js
+++ src/Player.js
@@ -6,13 +6,24 @@ class Player {
   };
 
   playCard() {
-    if (this.hand > 0) {
-      this.hand.shift();
-    };
+    console.log(  //before the function
+      "B centerPile.length", game.centerPile.length, 
+      "B this.hand", this.hand.length, 
+      "B game.playerA.hand", game.playerA.hand.length,
+    )
+    game.centerPile.push(this.hand.shift());
+    console.log(    //after the function fires
+      "A centerPile.length", game.centerPile.length, 
+      "A this.hand", this.hand.length, 
+      "A game.playerA.hand", game.playerA.hand.length,
+    )
   };
+  
 
   saveToStorage() {
     this.wins
   }
 
+
 };
+   // if (this.hand.length > 0) {

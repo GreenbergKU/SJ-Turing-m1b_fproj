a03f68320368909e0fcad82f3edd364dc11c5d35
Suzie Q <suzieqwolf@gmail.com>
2020-06-17 21:13:53 -0600

    Removes all comments but leaves console.logs

--- src/Player.js
+++ src/Player.js
@@ -13,23 +13,18 @@ class Player {
       "B this.hand", this.hand.length, 
       "B playerA hand", game.playerA.hand.length,
       "B playerB hand", game.playerB.hand.length,
-
-    ) //before the function
-
+    )
     game.centerPile.push(this.hand.shift());
-
     console.log(    
       "A centerPile.length", game.centerPile.length, 
       "A this.hand", this.hand.length, 
       "A playerA hand", game.playerA.hand.length,
-      "A playerB hand", game.playerB.hand.length,
-      
-    )  //after the function fires
+      "A playerB hand", game.playerB.hand.length,     
+    )
   };
   
   toggleSlap() {                                    
     console.log("toggleSlap")
-    //return
     this.slapped = !this.slapped; 
   };
   
@@ -39,25 +34,16 @@ class Player {
     console.log("updWins: player=", player, "this.wins=", this.wins, "game=", game)
     var savedSlapJackPlayers = [];
     this.saveToStorage(game, savedSlapJackPlayers);
-  }
-
-  // saveToStorage(player, savedPlayers) {        console.log("saveToStorage")
-
-  //   var stringifiedSlapJack = JSON.stringify({"id": player.id, "wins": player.wins});
-  //   localStorage.setItem('slap-jack: playerWins', stringifiedSlapJack);
-  // };
+  };
   
   saveToStorage(game, savedPlayers) {        
     console.log("saveToStorage");
     var savedPlayers = [];
     var playerA = {"id" : game.playerA.id, "wins": game.playerA.wins}; 
     var playerB = {"id" : game.playerB.id, "wins": game.playerB.wins};  
- // var stringifiedSlapJackSinglePlayer = JSON.stringify(player);
- // localStorage.setItem('slap-jack: playerWins', stringifiedSlapJackSinglePlayer)  
     savedPlayers.push(playerA, playerB); 
     var stringifiedSlapJackPlayers = JSON.stringify(savedPlayers);   
-    localStorage.setItem('slap-jack:playerWins', stringifiedSlapJackPlayers)
-  };
-  
+    localStorage.setItem('slap-jack:playerWins', stringifiedSlapJackPlayers);
+  }; 
 };
-   // if (this.hand.length > 0) { 
+
--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ document.addEventListener("keydown", delegateDealvsSlap);
 
 function delegateDealvsSlap(event) {
     console.log("@Handle: delegateDvsS", event)
-    // @handle => takeTurn, (Fully dependant on playerTurn!!)
-
     if (event.key === "q") {
         userDealCard(game.playerA);   
     };
@@ -20,17 +18,14 @@ function delegateDealvsSlap(event) {
         userSlapCard(game.playerB);    
     };
     if (game.gameOver && event.code === "Space") {
-        //document.querySelector('h1').classList.toggle("hidden"); 
         retrieveFromStorage();
-    }
+    };
 };
   
 function retrieveFromStorage() {
     console.log("@retrieveFromStorage")
     var player1, player2;
-    if (localStorage.getItem("slap-jack:playerWins") !== null  ) {
-        // (player1 = "playerA", 
-        // player1 = "playerB" ) :
+    if (localStorage.getItem("slap-jack:playerWins") !== null) {
         document.querySelector('h1').innerText = "2 PLAYER SLAP-JACK!"
         var player1 = JSON.parse(localStorage.getItem("slap-jack:playerWins"))[0].wins;
         var player2 = JSON.parse(localStorage.getItem("slap-jack:playerWins"))[1].wins;  
@@ -43,8 +38,6 @@ function setUpGame(player1, player2) {
     console.log('@setUpGame');    
      var playerA = new Player("playerA", player1);
      var playerB = new Player("playerB", player2);   
-    //var playerA = new Player(player1);
-    //var playerB = new Player(player2);
     game = new Game(playerA, playerB);
     cardCreation();
     displayWins();
@@ -80,20 +73,13 @@ function userDealCard(player) {
             game.checkForNoSlap();
         };
         if (game.endGameCondition && !game.gameOver) {
-            game.endGameCheck()
+            game.endGameCheck();
         };
         game.takeTurn();
-        displayGame(player);
-        // if (game.endGameCondition) {
-        //     game.lastChance ? game.checkLastChance() : game.endGameCheck();
-        // };
-        // game.takeTurn();
-        // displayGame(player);       
+        displayGame(player);    
     };
 };
-// if (game.endGameCondition) {
-        //     game.ForWinnercheck()
-        // };       
+    
 function userSlapCard(player) {
     console.log("@userSlapCard", player)
     if (!game.slapOccured) {
@@ -101,19 +87,14 @@ function userSlapCard(player) {
     };
     if (!game.gameOver) {
         game.endGameCondition ? game.endGameCheck(player) : game.checkWinConditions(player);
-        // game.endGameCondition ? game.checkLastChance(player) : game.checkWinConditions(player)
-        console.log("@userSlap: lastChance= ", game.lastChance,
-        "@userslap: game.endGameCond=", game.endGameCondition);
+        console.log("@userSlap: lastChance= ", game.lastChance);
     };
-    // game.endGameCondition ? game.checkForLastChance() :
-    // !game.lastChance ? game.checkWinConditions(player) : game.checkForWinner(player);   
 };
 
 function displayCard() {
     console.log("@displayCard()")
     document.getElementById(`${game.playerA.id}`).src="./assets/deckCard/back.png";
     document.getElementById(`${game.playerB.id}`).src="./assets/deckCard/back.png";
-    
     if (game.centerPile.length > 0) {
         var middlePile = document.getElementById("center-pile");
         var playerCard = game.centerPile.slice(-1);
@@ -122,10 +103,7 @@ function displayCard() {
     if (game.endGameCondition) {
         document.getElementById(`${game.playersTurn.id}`).src="./assets/deckCard/back.png";
         document.getElementById(`${game.underDog.id}`).src="";  
-    }; //else {
-    //     document.getElementById(`${game.playerA.id}`).src="./assets/deckCard/back.png";
-    //     document.getElementById(`${game.playerB.id}`).src="./assets/deckCard/back.png";
-    // }    
+    };   
 };
 
 function toggleTurnBorder() {
@@ -142,11 +120,8 @@ function displayMessage(player) {
         var other = player === game.playerA ? game.playerB : game.playerA;
         var goodSlapMsg = `${game.slap} ${player.id} takes the pile!`;
         var badSlapMsg = `${game.slap} ${player.id} forfeits a card to ${other.id}!`; 
-        // var winMsg =`${game.slap} ${player.id} YOU WIN!`; 
-        // var drawMsg = `It's a ${game.slap}, ${player.id}`; 
         header.classList.toggle('hidden');
-        game.gameOver ? displayGameOverMsg(player)
-            : header.innerText = game.goodSlap ? goodSlapMsg : badSlapMsg;
+        game.gameOver ? displayGameOverMsg(player) : header.innerText = game.goodSlap ? goodSlapMsg : badSlapMsg;
         clearMessage(3000);   
     };
 };
@@ -155,16 +130,8 @@ function displayGameOverMsg(player) {
     var header = document.querySelector('h1');
     var winMsg =`${game.slap} ${player.id} YOU WIN!`; 
     var drawMsg = `It's a ${game.slap}, ${player.id}`; 
-    //header.classList.toggle('hidden');
     header.innerText = game.slap === "WINNER!" ? winMsg : drawMsg;
 };
-    
-    // 
-    //     document.querySelector('h1').classList.toggle('hidden');
-    //     document.querySelector('h1').innerText =
-             
-    
-   
 
 function clearMessage(milliseconds) {
     console.log("@clearMessage")    
@@ -174,8 +141,7 @@ function clearMessage(milliseconds) {
         document.querySelector('h1').classList.toggle("hidden"); 
             if (centerImg.src.includes("again")) {
                 centerImg.src=""; 
-            };  
-         
+            };           
         }, milliseconds);
     };
 };
@@ -192,51 +158,4 @@ function shortcut() {
     game.shuffle(game.playerA.hand);
     game.checkGameStatus();
     displayGame(game.playersTurn);
-    // game.slap = undefined;
-    // game.lastChance = undefined;
-    // game.slapOccured = false;
-    // game.gameOver = undefined;
 };
-
-
-
-//********* RETRIEVE FROM STORAGE() notes *************** 
-//     var retrievedPlayers = localStorage.getItem("slap-jack:playerWins") 
-//     var savedSlapJackPlayers = JSON.parse(retrievedPlayers);
-//     var playerA = savedSlapJackPlayers[0];
-//     var playerB = savedSlapJackPlayers[1];
-//     setUpGame(playerA, playerB); 
-// }
-   // localStorage.getItem("slap-jack:playerWins") !== null ?  
-    //     ( playerA = JSON.parse(localStorage.getItem("slap-jack:playerWins"))[0],
-    //     playerB = JSON.parse(localStorage.getItem("slap-jack:playerWins"))[1]   
-    //     ) :  "playerA", "playerB";        
-    // setUpGame(playerA, playerB);
-    // };
-     
-
-//********* DISPLAY CARD() notes 
-// var imgAllTags = document.getElementsByTagName("img");
-// var card = game.centerPile.slice(-1);
-// game.slapOccured ? imgAllTags[1].src = "" :
-// imgAllTags[1].src = `${card[0].filepath}.png`;
-
-//  *********** DISPLAY MESSAGE() notes   
-// document.querySelector('h1').innerText = game.goodSlap ? goodSlapMsg : badSlapMsg;
-// 
-
-// ********* CLEAR MESSAGE() notes
-// MDN- setTIMEOUT 
-//document.getElementById('centerPile').src="";
-// function stateChange(newState) {
-//     setTimeout(function () {
-//         if (newState == -1) {
-//             alert('VIDEO HAS STOPPED');
-//         }
-//     }, 5000);
-// }
-        //header.innerText="";
-        //header.style.backgroundColor="transparent";
-        //header.style.border="transparent";
-        // header.classList.toggle("hidden");
-        // var header = document.querySelector('h1');
